64df062bde35f94748460df86924724d092ca0e8
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-06-30 21:22:34 +0100

    Hook into events

--- lua/job_dependent_behaviour/base.lua
+++ lua/job_dependent_behaviour/base.lua
@@ -18,6 +18,21 @@ function JBD.new()
     return behaviour
 end
 
+function JBD.UpdateBehaviours()
+    for _, behaviour in pairs( JBD.behaviours ) do
+        behaviour:UpdateValue()
+    end
+end
+
+hook.Add( "DarkRPVarChanged", "cfc_job_dependent_behaviour", function( ply, varName, prev, new )
+    if varName ~= "job" then return end
+
+    -- This function is called before the job is actually set
+    timer.Simple( 0, JBD.UpdateBehaviours )
+end )
+
+hook.Add( "PlayerDisconnected", "cfc_job_dependent_behaviour", JBD.UpdateBehaviours )
+
 -- Include inbuilt behaviours
 local _, files = file.Find( "job_dependent_behaviour/behaviours/*", "LUA" )
 
